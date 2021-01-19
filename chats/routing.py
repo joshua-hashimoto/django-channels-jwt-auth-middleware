1e6ea843cae0a9b7d55564bd251adeecc8e82eed
dropscar <dropscar041205@gmail.com>
2021-01-19 13:01:49 +0900

    delete build process projects

--- chats/routing.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from .consumers import ChatConsumer
-
-app_name = 'chats'
-
-websocket_urlpatterns = [
-    path('ws/chat/<str:room_name>/', ChatConsumer.as_asgi()),
-]
--- django-channels-jwt-auth-middleware/__init__.py
+++
@@ -1,3 +0,0 @@
-__author__ = 'Joshua Hashimoto'
-__email__ = 'onandgo.dev@gmail.com'
-__version__ = '1.0.0'
